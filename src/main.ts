f51f59be898121a46740ba502fcf99b031344ce1
MrBeeni <mubeen4429@gmail.com>
2023-10-13 08:23:20 +0500

    final code

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('CRUD App')
+    .setDescription('A practice CRUD Application!!')
+    .setVersion('1.0')
+    .addTag('CRUD')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(7000);
+}
+bootstrap();
